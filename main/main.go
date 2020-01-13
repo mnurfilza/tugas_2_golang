fba93efae30b76ff9a7c3c8cb90a83c411647365
mnurfilza <choirfilza@gmail.com>
2020-01-13 16:54:54 +0700

    first commit

---
+++ main/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "belajargolang/tugas2"
+
+func main() {
+	tugas2.Tugas2(5)
+}
---
+++ tugas.go
@@ -0,0 +1,11 @@
+package tugas2
+
+import "fmt"
+
+func Tugas2(nomer int) {
+	if nomer%2 == 0 {
+		fmt.Println("Bilangan genap")
+	} else {
+		fmt.Println("Bilangan Ganjil")
+	}
+}
